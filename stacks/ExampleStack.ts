8bcfcb3a887d2d214d14dc73c92f931cb9dd9ffd
Clement Marcilhacy <clementmarcilhacy@gmail.com>
2023-10-13 16:33:59 +0200

    feat: remove addfixtures route

--- stacks/ExampleStack.ts
+++ stacks/ExampleStack.ts
@@ -41,7 +41,6 @@ export function ExampleStack({ stack }: StackContext) {
     },
     routes: {
       "POST /": "packages/functions/src/lambda.main",
-      "POST /add-fixtures": "packages/functions/src/addFixtures.main",
       "POST /add-user-rankings": "packages/functions/src/addUserRankings.main",
       "POST /simulate-matches": "packages/functions/src/simulateMatches.main",
       "GET /get-matches": "packages/functions/src/getMatches.main",
